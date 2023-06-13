8959cbe175331c88f160afd6037fe626cc383a2b
ryan-su-12 <115808735+ryan-su-12@users.noreply.github.com>
2023-06-12 23:26:08 -0400

    Add files via upload
    yo!

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+/* darkMode */
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+
+};
+
+ScrollReveal({
+    reset: true,
+    distance : '80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+
+
