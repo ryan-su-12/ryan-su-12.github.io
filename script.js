d007a87f6753475c2a1196d60afbe3d96ebef18b
ryan-su-12 <21rzs@queensu.ca>
2023-09-24 14:40:15 -0400

    new pfp

--- script.js
+++ script.js
@@ -64,3 +64,12 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
 
 
+/*Retreve the file for my resume  */
+
+document.getElementById("pdfButton").addEventListener("click", function() {
+    // Replace 'your-pdf-file.pdf' with the actual path to your PDF file
+    var pdfUrl = 'ResumeFall2023.pdf';
+    
+    // Open the PDF in a new window or tab
+    window.open(pdfUrl, '_resume');
+});
---
+++ test.js
@@ -0,0 +1,7 @@
+document.getElementById("pdfButton").addEventListener("click", function() {
+    // Replace 'your-pdf-file.pdf' with the actual path to your PDF file
+    var pdfUrl = 'ResumeFall2023.pdf';
+    
+    // Open the PDF in a new window or tab
+    window.open(pdfUrl, '_try');
+});
